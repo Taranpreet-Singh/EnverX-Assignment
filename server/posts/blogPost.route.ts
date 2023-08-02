167a505ca8034781f83e583f39d0ab5a40deb69d
Taranpreet Singh <taranpreetsingh@getrealhealth.com>
2023-08-02 22:37:33 +0530

    Fix: Corrected validation for Update and Delete Operation

--- server/posts/blogPost.route.ts
+++ server/posts/blogPost.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { blogPostController } from "./blogPost.controller";
 import { validate } from "../middleware/validation.middleware";
-import { blogPostCreateRequestSchema, blogPostUpdateRequestSchema } from "../validation/blogPost.validation";
+import { blogPostCreateRequestSchema, blogPostParamsOnlyRequestSchema, blogPostUpdateRequestSchema } from "../validation/blogPost.validation";
 
 
 export const router = Router();
 
 router.get('/', blogPostController.getAllBlogPosts)
-router.get('/:id', blogPostController.getBlogPostById)
+router.get('/:id', validate(blogPostParamsOnlyRequestSchema), blogPostController.getBlogPostById)
 router.post('/', validate(blogPostCreateRequestSchema), blogPostController.createBlogPost); 
-router.put('/:id', validate(blogPostCreateRequestSchema), blogPostController.updateBlogPost);
-router.delete('/:id', blogPostController.deleteBlogPost);
+router.put('/:id', validate(blogPostUpdateRequestSchema), blogPostController.updateBlogPost);
+router.delete('/:id', validate(blogPostParamsOnlyRequestSchema), blogPostController.deleteBlogPost);
--- server/validation/blogPost.validation.ts
+++ server/validation/blogPost.validation.ts
@@ -11,13 +11,21 @@ export const blogPostCreateRequestSchema = yup.object({
 })
 
 export const blogPostUpdateRequestSchema = yup.object({
-    body: yup.object({
+    params: yup.object({
         postId: yup.string()
-            .required("Post Id is required to update!")
             .uuid(),
+    }),
+    body: yup.object({
         title: yup.string()
             .required("Enter a valid title for post")
             .min(3, "Mininum 3 characters!"),
         description: yup.string()
+    })
+})
+
+export const blogPostParamsOnlyRequestSchema = yup.object({
+    params: yup.object({
+        postId: yup.string()
+            .uuid(),
     })
 })
